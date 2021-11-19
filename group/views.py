4c377a630426f971e2f4478a6dea08c9120c7353
ainsalleh1 <94127918+ainsalleh1@users.noreply.github.com>
2021-11-20 01:28:33 +0800

    update

--- group/views.py
+++ group/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 # from LOGIN.models import Feed, Booking, Workshop, Group, Member 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, request
 from django import forms
 # from .forms import CreateInDiscussion, PersonForm, UserUpdateForm
 from django.urls import reverse
@@ -12,8 +12,9 @@ from django.core.files.storage import FileSystemStorage
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from cryptography.fernet import Fernet
-# from .models import Person
 
+from group.models import Group
+# from .models import Person
 
 
 #group
@@ -32,12 +33,13 @@ def group(request):
         Group(Name=Name,About=About,Media=Media).save(),
         messages.success(request,'The new group ' + request.POST['Name'] + " is create succesfully..!")
         return render(request,'group.html')
+
     else :
         return render(request,'group.html')
 
 def myGroup(request):
-    #try:
-    #    group=Group.objects.filter(Name=request.session['Name'])
-        return render(request,'MyGroup.html')#{'group':group})
-    #except Group.DoesNotExist:
-     #   raise Http404('Data does not exist')
+    try:
+        #group=Group.objects.filter(Name=request.session['Name'])
+        return render(request,'MyGroup.html',{'group':group})
+    except Group.DoesNotExist:
+       raise Http404('Data does not exist')
