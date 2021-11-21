a51f1647c9be032f546b638c16963e3cb1f1b198
ainsalleh1 <94127918+ainsalleh1@users.noreply.github.com>
2021-11-21 22:10:26 +0800

    update

--- igrowKMS/settings.py
+++ igrowKMS/settings.py
@@ -168,17 +168,17 @@ STATICFILES_DIRS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-    #    'rest_framework.permissions.IsAuthenticated',
-    #    'rest_framework.permissions.IsAdminUser',
-    #   'rest_framework.permissions.AllowAny'
+        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser',
+       'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES':[
-    #    'rest_framework.authentication.BasicAuthentication',
-    #    'rest_framework.authentication.SessionAuthentication',
-    #    'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Default.primary.key.field.type
-# DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
